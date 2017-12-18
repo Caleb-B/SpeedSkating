c4aa5f9ee68d4021132836f8021485861daa28d8
CFSL <CFSL@CFSL-660912.gateway.mts.net>
2017-12-18 14:01:47 -0600

    created a getSum() method

--- SpeedSkating/src/speedSkater.java
+++ SpeedSkating/src/speedSkater.java
@@ -59,25 +59,53 @@
 		 return timeList[idx].toString();
 	 } // end getTime
 	 
+	//*****************************************************
+	// Purpose: get the average time for all races
+	// Interface: IN: none
+	// Returns: avgStr
+	// *****************************************************
 	 public String getAvgTime() {
-		 int sum = 0;			// total sum of all times in timeList, in seconds
 		 int avg;				// average time in seconds
 		 
 		 String avgStr;			// average time in minutes and seconds, as string
-		 for (int i = 0; i < count; i++) {
-			 sum += timeList[i].getMinutes() * 60;
-			 sum += timeList[i].getSeconds();
-		 } // end for int i
 		 
-		 avg = sum / count;
+		 avg = this.getSum() / count;
 		 
 		 avgStr = String.format("%02d:%02d", (avg / 60), (avg % 60));
 		 
 		 return avgStr;
 	 } // end getAvgTime
 	 
+	//*****************************************************
+	// Purpose: get the sum of times for all races
+	// Interface: IN: none
+	// Returns: sum
+	// *****************************************************
+	 public int getSum() {
+		 int sum = 0;			// total sum of all times in timeList, in seconds
+		 
+		 for (int i = 0; i < count; i++) {
+			 sum += timeList[i].getMinutes() * 60;
+			 sum += timeList[i].getSeconds();
+		 } // end for int i
+		 
+		 return sum;
+	 } // end getSum
+	 
+	 public String getAvgSpeed() {
+		 String avgSpeed;
+		 avgSpeed = "";
+		 return avgSpeed;
+	 } // end for getAvgSpeed
+	 
  	// ********** mutators **********
 	 
+	//*****************************************************
+	// Purpose: adds a time to timeList
+	// Interface: IN: index (int),
+	//				time (Time)
+	// Returns: none
+	// *****************************************************
 	 public void addTime(int idx, Time t) {
 		 if (count < timeList.length) {
 			 timeList[count] = t;
